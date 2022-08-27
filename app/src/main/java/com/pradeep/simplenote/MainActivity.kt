c139f6024073ef546d74a496c9401808eb366c7a
pradeep <pradeephr03@icloud.com>
2022-08-27 22:53:37 +0530

    ui changes

--- app/src/main/java/com/pradeep/simplenote/MainActivity.kt
+++ app/src/main/java/com/pradeep/simplenote/MainActivity.kt
@@ -3,6 +3,7 @@ package com.pradeep.simplenote
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
